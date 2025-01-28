aee6a63886e8a99dc6b3368f2bc4efeb64698151
gnillor <gump.zeng@gmail.com>
2025-01-28 21:31:38 +0800

    Add longest palindrome solution.

---
+++ 5.最长回文子串.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=java
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return null;
+        }
+
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = getPalindromeLen(s, i, i);
+            int len2 = getPalindromeLen(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    private int getPalindromeLen(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
+// @lc code=end
