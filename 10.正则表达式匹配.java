ab376934a9515e0c38c4a8a18a1695022a643fc7
gnillor <gump.zeng@gmail.com>
2025-02-08 16:39:26 +0800

    Refactor regular expression matching solution
    - Renamed variables for improved readability
    - Simplified variable names in dynamic programming approach
    - Added minor code formatting improvements
    - Maintained existing matching logic and algorithm structure

--- 10.正则表达式匹配.java
+++ 10.正则表达式匹配.java
@@ -7,25 +7,26 @@
 // @lc code=start
 class Solution {
     public boolean isMatch(String s, String p) {
-        boolean[][] f = new boolean[s.length() + 1][p.length() + 1];
-        f[0][0] = true;
+        boolean[][] match = new boolean[s.length() + 1][p.length() + 1];
+        match[0][0] = true;
+
         for (int i = 0; i <= s.length(); i++) {
             for (int j = 1; j <= p.length(); j++) {
                 if (p.charAt(j - 1) == '*') {
                     if (matches(s, p, i, j - 1)) {
-                        f[i][j] = f[i - 1][j] || f[i][j - 2];
+                        match[i][j] = match[i - 1][j] || match[i][j - 2];
                     } else {
-                        f[i][j] = f[i][j - 2];
+                        match[i][j] = match[i][j - 2];
                     }
                 } else {
                     if (matches(s, p, i, j)) {
-                        f[i][j] = f[i - 1][j - 1];
+                        match[i][j] = match[i - 1][j - 1];
                     }
                 }
             }
         }
 
-        return f[s.length()][p.length()];
+        return match[s.length()][p.length()];
     }
 
     private boolean matches(String s, String p, int i, int j) {
@@ -36,6 +37,7 @@ class Solution {
         if (p.charAt(j - 1) == '.') {
             return true;
         }
+
         return s.charAt(i - 1) == p.charAt(j - 1);
     }
 }
