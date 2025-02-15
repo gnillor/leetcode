bf00165263f375b18e6b9983d15efa336491098a
gnillor <gump.zeng@gmail.com>
2025-02-15 18:11:45 +0800

    feat: 实现合并两个有序链表的解决方案
    - 使用虚拟头节点简化边界情况处理
    - 通过迭代方式合并两个链表
    - 处理剩余节点的连接

---
+++ 21.合并两个有序链表.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                node.next = list1;
+                list1 = list1.next;
+            } else {
+                node.next = list2;
+                list2 = list2.next;
+            }
+
+            node = node.next;
+        }
+
+        while (list1 != null) {
+            node.next = list1;
+            list1 = list1.next;
+            node = node.next;
+        }
+
+        while (list2 != null) {
+            node.next = list2;
+            list2 = list2.next;
+            node = node.next;
+        }
+
+        return dummy.next;
+    }
+}
+// @lc code=end
