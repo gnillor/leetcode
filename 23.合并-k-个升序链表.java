d4a7138c31cc56b0f2f801bde1af8d1e284c378b
gnillor <gump.zeng@gmail.com>
2025-02-21 18:04:58 +0800

    Add 23 solution.

---
+++ 23.合并-k-个升序链表.java
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=java
+ *
+ * [23] 合并 K 个升序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        ListNode ret = lists[0];
+        for (int i = 1; i < lists.length; i++) {
+            ret = merge2Lists(ret, lists[i]);
+        }
+
+        return ret;
+    }
+
+    private ListNode merge2Lists(ListNode list1, ListNode list2) {
+        ListNode ret = new ListNode(0);
+        ListNode curr = ret;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        while (list1 != null) {
+            curr.next = list1;
+            list1 = list1.next;
+            curr = curr.next;
+        }
+        while (list2 != null) {
+            curr.next = list2;
+            list2 = list2.next;
+            curr = curr.next;
+        }
+
+        return ret.next;
+    }
+}
+// @lc code=end
